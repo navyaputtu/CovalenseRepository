e7e89a47e7bf29e81ffd2cc0b88f65dc72039b4c
navya.puttu <navya.puttu@NavyaPuttu.covalensedigital.net>
2021-11-22 13:06:59 +0530

    commit-1.0-done with http control actions

---
+++ SpringBootReST-0/src/main/java/com/cov/SpringBootReSt0Application.java
@@ -0,0 +1,13 @@
+package com.cov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReSt0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReSt0Application.class, args);
+	}
+
+}
